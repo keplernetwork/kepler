mwcproject/mwc-node

Reduce number of unwwaps in api crate  (#2681)
* Reduce number of unwwaps in api crate

* Format use section

api/src/auth.rs
@@ -13,19 +13,25 @@
 // limitations under the License.
 
 use crate::router::{Handler, HandlerObj, ResponseFuture};
+use crate::web::response;
 use futures::future::ok;
 use hyper::header::{HeaderValue, AUTHORIZATION, WWW_AUTHENTICATE};
 use hyper::{Body, Request, Response, StatusCode};
 use ring::constant_time::verify_slices_are_equal;
 
+lazy_static! {
+	pub static ref GRIN_BASIC_REALM: HeaderValue =
+		HeaderValue::from_str("Basic realm=GrinAPI").unwrap();
+}
+
 // Basic Authentication Middleware
 pub struct BasicAuthMiddleware {
 	api_basic_auth: String,
-	basic_realm: String,
+	basic_realm: &'static HeaderValue,
 }
 
 impl BasicAuthMiddleware {
-	pub fn new(api_basic_auth: String, basic_realm: String) -> BasicAuthMiddleware {
+	pub fn new(api_basic_auth: String, basic_realm: &'static HeaderValue) -> BasicAuthMiddleware {
 		BasicAuthMiddleware {
 			api_basic_auth,
 			basic_realm,
@@ -39,8 +45,12 @@ impl Handler for BasicAuthMiddleware {
 		req: Request<Body>,
 		mut handlers: Box<dyn Iterator<Item = HandlerObj>>,
 	) -> ResponseFuture {
+		let next_handler = match handlers.next() {
+			Some(h) => h,
+			None => return response(StatusCode::INTERNAL_SERVER_ERROR, "no handler found"),
+		};
 		if req.method().as_str() == "OPTIONS" {
-			return handlers.next().unwrap().call(req, handlers);
+			return next_handler.call(req, handlers);
 		}
 		if req.headers().contains_key(AUTHORIZATION)
 			&& verify_slices_are_equal(
@@ -49,7 +59,7 @@ impl Handler for BasicAuthMiddleware {
 			)
 			.is_ok()
 		{
-			handlers.next().unwrap().call(req, handlers)
+			next_handler.call(req, handlers)
 		} else {
 			// Unauthorized 401
 			unauthorized_response(&self.basic_realm)
@@ -57,13 +67,10 @@ impl Handler for BasicAuthMiddleware {
 	}
 }
 
-fn unauthorized_response(basic_realm: &str) -> ResponseFuture {
+fn unauthorized_response(basic_realm: &HeaderValue) -> ResponseFuture {
 	let response = Response::builder()
 		.status(StatusCode::UNAUTHORIZED)
-		.header(
-			WWW_AUTHENTICATE,
-			HeaderValue::from_str(basic_realm).unwrap(),
-		)
+		.header(WWW_AUTHENTICATE, basic_realm)
 		.body(Body::empty())
 		.unwrap();
 	Box::new(ok(response))

api/src/client.rs
@@ -136,9 +136,8 @@ fn build_request<'a>(
 			.into()
 	})?;
 	let mut builder = Request::builder();
-	if api_secret.is_some() {
-		let basic_auth =
-			"Basic ".to_string() + &to_base64(&("grin:".to_string() + &api_secret.unwrap()));
+	if let Some(api_secret) = api_secret {
+		let basic_auth = format!("Basic {}", to_base64(&format!("grin:{}", api_secret)));
 		builder.header(AUTHORIZATION, basic_auth);
 	}
 
@@ -224,6 +223,7 @@ fn send_request_async(req: Request<Body>) -> Box<dyn Future<Item = String, Error
 
 fn send_request(req: Request<Body>) -> Result<String, Error> {
 	let task = send_request_async(req);
-	let mut rt = Runtime::new().unwrap();
+	let mut rt =
+		Runtime::new().context(ErrorKind::Internal("can't create Tokio runtime".to_owned()))?;
 	Ok(rt.block_on(task)?)
 }

api/src/lib.rs
@@ -39,7 +39,7 @@ mod rest;
 mod router;
 mod types;
 
-pub use crate::auth::BasicAuthMiddleware;
+pub use crate::auth::{BasicAuthMiddleware, GRIN_BASIC_REALM};
 pub use crate::handlers::start_rest_apis;
 pub use crate::rest::*;
 pub use crate::router::*;

keplernetwork/kepler

api/src/auth.rs
@@ -13,19 +13,25 @@
 // limitations under the License.
 
 use crate::router::{Handler, HandlerObj, ResponseFuture};
+use crate::web::response;
 use futures::future::ok;
 use hyper::header::{HeaderValue, AUTHORIZATION, WWW_AUTHENTICATE};
 use hyper::{Body, Request, Response, StatusCode};
 use ring::constant_time::verify_slices_are_equal;
 
+lazy_static! {
+	pub static ref KEPLER_BASIC_REALM: HeaderValue =
+		HeaderValue::from_str("Basic realm=KeplerAPI").unwrap();
+}
+
 // Basic Authentication Middleware
 pub struct BasicAuthMiddleware {
 	api_basic_auth: String,
-	basic_realm: String,
+	basic_realm: &'static HeaderValue,
 }
 
 impl BasicAuthMiddleware {
-	pub fn new(api_basic_auth: String, basic_realm: String) -> BasicAuthMiddleware {
+	pub fn new(api_basic_auth: String, basic_realm: &'static HeaderValue) -> BasicAuthMiddleware {
 		BasicAuthMiddleware {
 			api_basic_auth,
 			basic_realm,
@@ -39,8 +45,12 @@ impl Handler for BasicAuthMiddleware {
 		req: Request<Body>,
 		mut handlers: Box<dyn Iterator<Item = HandlerObj>>,
 	) -> ResponseFuture {
+		let next_handler = match handlers.next() {
+			Some(h) => h,
+			None => return response(StatusCode::INTERNAL_SERVER_ERROR, "no handler found"),
+		};
 		if req.method().as_str() == "OPTIONS" {
-			return handlers.next().unwrap().call(req, handlers);
+			return next_handler.call(req, handlers);
 		}
 		if req.headers().contains_key(AUTHORIZATION)
 			&& verify_slices_are_equal(
@@ -49,7 +59,7 @@ impl Handler for BasicAuthMiddleware {
 			)
 			.is_ok()
 		{
-			handlers.next().unwrap().call(req, handlers)
+			next_handler.call(req, handlers)
 		} else {
 			// Unauthorized 401
 			unauthorized_response(&self.basic_realm)
@@ -57,13 +67,10 @@ impl Handler for BasicAuthMiddleware {
 	}
 }
 
-fn unauthorized_response(basic_realm: &str) -> ResponseFuture {
+fn unauthorized_response(basic_realm: &HeaderValue) -> ResponseFuture {
 	let response = Response::builder()
 		.status(StatusCode::UNAUTHORIZED)
-		.header(
-			WWW_AUTHENTICATE,
-			HeaderValue::from_str(basic_realm).unwrap(),
-		)
+		.header(WWW_AUTHENTICATE, basic_realm)
 		.body(Body::empty())
 		.unwrap();
 	Box::new(ok(response))

api/src/client.rs
@@ -136,9 +136,8 @@ fn build_request<'a>(
 			.into()
 	})?;
 	let mut builder = Request::builder();
-	if api_secret.is_some() {
-		let basic_auth =
-			"Basic ".to_string() + &to_base64(&("kepler:".to_string() + &api_secret.unwrap()));
+	if let Some(api_secret) = api_secret {
+		let basic_auth = format!("Basic {}", to_base64(&format!("kepler:{}", api_secret)));
 		builder.header(AUTHORIZATION, basic_auth);
 	}
 
@@ -224,6 +223,7 @@ fn send_request_async(req: Request<Body>) -> Box<dyn Future<Item = String, Error
 
 fn send_request(req: Request<Body>) -> Result<String, Error> {
 	let task = send_request_async(req);
-	let mut rt = Runtime::new().unwrap();
+	let mut rt =
+		Runtime::new().context(ErrorKind::Internal("can't create Tokio runtime".to_owned()))?;
 	Ok(rt.block_on(task)?)
 }

api/src/lib.rs
@@ -39,7 +39,7 @@ mod rest;
 mod router;
 mod types;
 
-pub use crate::auth::BasicAuthMiddleware;
+pub use crate::auth::{BasicAuthMiddleware, KEPLER_BASIC_REALM};
 pub use crate::handlers::start_rest_apis;
 pub use crate::rest::*;
 pub use crate::router::*;