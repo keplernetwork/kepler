mwcproject/mwc-node

fix: try to fix issue #2585 by adding block cleanup from db directly. (#2815)
* fix: try to fix issue #2585 by adding block cleanup from db directly.

Signed-off-by: Mike Tang <daogangtang@gmail.com>

* use another effective algorithm to do old block and short-lived block cleaup.

Signed-off-by: Mike Tang <daogangtang@gmail.com>

* 1. rename iter_lived_blocks to blocks_iter;
2. comments and iterator calling optimiztions.

Signed-off-by: Mike Tang <daogangtang@gmail.com>

* Fix locking bug when calling is_on_current_chain() in batch.blocks_iter just by removing it.
Because "we want to delete block older (i.e. lower height) than tail.height".

Signed-off-by: Mike Tang <daogangtang@gmail.com>

chain/src/store.rs
@@ -22,7 +22,7 @@ use crate::types::Tip;
 use crate::util::secp::pedersen::Commitment;
 use croaring::Bitmap;
 use grin_store as store;
-use grin_store::{option_to_not_found, to_key, Error};
+use grin_store::{option_to_not_found, to_key, Error, SerIterator};
 use std::sync::Arc;
 
 const STORE_SUBPATH: &'static str = "chain";
@@ -378,6 +378,13 @@ impl<'a> Batch<'a> {
 			db: self.db.child()?,
 		})
 	}
+
+	/// An iterator to all block in db
+	pub fn blocks_iter(&self) -> Result<SerIterator<Block>, Error> {
+		let key = to_key(BLOCK_PREFIX, &mut "".to_string().into_bytes());
+		self.db.iter(&key)
+	}
+	
 }
 
 /// An iterator on blocks, from latest to earliest, specialized to return

keplernetwork/kepler

chain/src/store.rs
@@ -22,7 +22,7 @@ use crate::types::Tip;
 use crate::util::secp::pedersen::Commitment;
 use croaring::Bitmap;
 use kepler_store as store;
-use kepler_store::{option_to_not_found, to_key, Error};
+use kepler_store::{option_to_not_found, to_key, Error, SerIterator};
 use std::sync::Arc;
 
 const STORE_SUBPATH: &'static str = "chain";
@@ -377,6 +377,12 @@ impl<'a> Batch<'a> {
 		Ok(Batch {
 			db: self.db.child()?,
 		})
+	}
+
+	/// An iterator to all block in db
+	pub fn blocks_iter(&self) -> Result<SerIterator<Block>, Error> {
+		let key = to_key(BLOCK_PREFIX, &mut "".to_string().into_bytes());
+		self.db.iter(&key)
 	}
 }
 