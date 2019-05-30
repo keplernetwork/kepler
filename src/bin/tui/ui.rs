mwcproject/mwc-node

Relax our p2p rules around matching protocol versions (#2764)

src/bin/tui/ui.rs
@@ -88,7 +88,11 @@ impl UI {
 
 		let mut title_string = StyledString::new();
 		title_string.append(StyledString::styled(
-			format!("Grin Version {}", built_info::PKG_VERSION),
+			format!(
+				"Grin Version {} (protocol version: {})",
+				built_info::PKG_VERSION,
+				Server::protocol_version()
+			),
 			Color::Dark(BaseColor::Green),
 		));
 

keplernetwork/kepler

src/bin/tui/ui.rs
@@ -88,7 +88,11 @@ impl UI {
 
 		let mut title_string = StyledString::new();
 		title_string.append(StyledString::styled(
-			format!("Kepler Version {}", built_info::PKG_VERSION),
+			format!(
+				"Kepler Version {} (protocol version: {})",
+				built_info::PKG_VERSION,
+				Server::protocol_version()
+			),
 			Color::Dark(BaseColor::Green),
 		));
 