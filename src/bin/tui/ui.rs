mwcproject/mwc-node

Protocol version type safety (#2811)
* add type safety for protocol_version

* cleanup tui for protocol version

* cleanup

src/bin/tui/ui.rs
@@ -85,7 +85,7 @@ impl UI {
 		let mut title_string = StyledString::new();
 		title_string.append(StyledString::styled(
 			format!(
-				"Grin Version {} (protocol version: {})",
+				"Grin Version {} (proto: {})",
 				built_info::PKG_VERSION,
 				Server::protocol_version()
 			),

keplernetwork/kepler

src/bin/tui/ui.rs
@@ -85,7 +85,7 @@ impl UI {
 		let mut title_string = StyledString::new();
 		title_string.append(StyledString::styled(
 			format!(
-				"Kepler Version {} (protocol version: {})",
+				"Kepler Version {} (proto: {})",
 				built_info::PKG_VERSION,
 				Server::protocol_version()
 			),