mwcproject/mwc-node

Return Result from methods of ChainAdapter (#2722)
Most of the methods return nothing or bool which is used to decide if a
sender of a message should be banned or not. However underlying chain
implementation may fail so we need a way to reflect this fact in API.

Also it allows to reduce number of unwraps and makes the code more robust.

p2p/src/lib.rs
@@ -29,6 +29,7 @@ use lmdb_zero as lmdb;
 
 #[macro_use]
 extern crate grin_core as core;
+use grin_chain as chain;
 use grin_util as util;
 
 #[macro_use]

keplernetwork/kepler

p2p/src/lib.rs
@@ -29,6 +29,7 @@ use lmdb_zero as lmdb;
 
 #[macro_use]
 extern crate kepler_core as core;
+use kepler_chain as chain;
 use kepler_util as util;
 
 #[macro_use]