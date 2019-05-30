mwcproject/mwc-node

Set TUI FPS (#2783)
New version of Cursive allows to set fps, the existing  set_autorefresh sets 30 fps. This change decreases CPU usage from 50-65% to 15-25% on my machine.

src/bin/tui/ui.rs
@@ -117,7 +117,7 @@ impl UI {
 				.send(ControllerMessage::Shutdown)
 				.unwrap();
 		});
-		grin_ui.cursive.set_autorefresh(true);
+		grin_ui.cursive.set_fps(3);
 		grin_ui
 	}
 

keplernetwork/kepler

src/bin/tui/ui.rs
@@ -117,7 +117,7 @@ impl UI {
 				.send(ControllerMessage::Shutdown)
 				.unwrap();
 		});
-		kepler_ui.cursive.set_autorefresh(true);
+		kepler_ui.cursive.set_fps(3);
 		kepler_ui
 	}
 