mwcproject/mwc-node

Header version cleanup (#2809)
* introduce HeaderVersion (was u16) for type safety
cleanup pow ser/deser (version unused)

* fixup tests for HeaderVersion

* validate header version during header deserialization

chain/src/pipe.rs
@@ -341,7 +341,7 @@ fn validate_header(header: &BlockHeader, ctx: &mut BlockContext<'_>) -> Result<(
 	// check version, enforces scheduled hard fork
 	if !consensus::valid_header_version(header.height, header.version) {
 		error!(
-			"Invalid block header version received ({}), maybe update Grin?",
+			"Invalid block header version received ({:?}), maybe update Grin?",
 			header.version
 		);
 		return Err(ErrorKind::InvalidBlockVersion(header.version).into());

keplernetwork/kepler

chain/src/pipe.rs
@@ -341,7 +341,7 @@ fn validate_header(header: &BlockHeader, ctx: &mut BlockContext<'_>) -> Result<(
 	// check version, enforces scheduled hard fork
 	if !consensus::valid_header_version(header.height, header.version) {
 		error!(
-			"Invalid block header version received ({}), maybe update Kepler?",
+			"Invalid block header version received ({:?}), maybe update Kepler?",
 			header.version
 		);
 		return Err(ErrorKind::InvalidBlockVersion(header.version).into());