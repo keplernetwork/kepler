mwcproject/mwc-node

Implement graceful shutdown (#2812)
* Remove stop status mutex
* remove some deadlocks
* Rewrite stop channel handling
* fix deadlock in peers object
* add missing test fixes

p2p/tests/peer_handshake.rs
@@ -16,7 +16,7 @@ use grin_core as core;
 use grin_p2p as p2p;
 
 use grin_util as util;
-use grin_util::{Mutex, StopState};
+use grin_util::StopState;
 
 use std::net::{SocketAddr, TcpListener, TcpStream};
 use std::sync::Arc;
@@ -56,7 +56,7 @@ fn peer_handshake() {
 			p2p_config.clone(),
 			net_adapter.clone(),
 			Hash::from_vec(&vec![]),
-			Arc::new(Mutex::new(StopState::new())),
+			Arc::new(StopState::new()),
 		)
 		.unwrap(),
 	);

keplernetwork/kepler

p2p/tests/peer_handshake.rs
@@ -16,7 +16,7 @@ use kepler_core as core;
 use kepler_p2p as p2p;
 
 use kepler_util as util;
-use kepler_util::{Mutex, StopState};
+use kepler_util::StopState;
 
 use std::net::{SocketAddr, TcpListener, TcpStream};
 use std::sync::Arc;
@@ -56,7 +56,7 @@ fn peer_handshake() {
 			p2p_config.clone(),
 			net_adapter.clone(),
 			Hash::from_vec(&vec![]),
-			Arc::new(Mutex::new(StopState::new())),
+			Arc::new(StopState::new()),
 		)
 		.unwrap(),
 	);