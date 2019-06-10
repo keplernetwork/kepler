mwcproject/mwc-node

Report reorg depth in block status (#2839)
* Calculate reorg depth in BlockStatus::Reorg enum member

* rustfmt

* Fix reorg height calculation and implement reorg test

* rustfmt

* Report reorg depth in webhook payload

* Add optional depth field to the block webhook JSON reply

chain/tests/mine_simple_chain.rs
@@ -26,9 +26,11 @@ use self::keychain::{ExtKeychain, ExtKeychainPath, Keychain};
 use self::util::{RwLock, StopState};
 use chrono::Duration;
 use grin_chain as chain;
+use grin_chain::{BlockStatus, ChainAdapter, Options};
 use grin_core as core;
 use grin_keychain as keychain;
 use grin_util as util;
+use std::cell::RefCell;
 use std::fs;
 use std::sync::Arc;
 
@@ -49,6 +51,41 @@ fn setup(dir_name: &str, genesis: Block) -> Chain {
 		false,
 	)
 	.unwrap()
+}
+
+/// Adapter to retrieve last status
+pub struct StatusAdapter {
+	pub last_status: RwLock<Option<BlockStatus>>,
+}
+
+impl StatusAdapter {
+	pub fn new(last_status: RwLock<Option<BlockStatus>>) -> Self {
+		StatusAdapter { last_status }
+	}
+}
+
+impl ChainAdapter for StatusAdapter {
+	fn block_accepted(&self, _b: &Block, status: BlockStatus, _opts: Options) {
+		*self.last_status.write() = Some(status);
+	}
+}
+
+/// Creates a `Chain` instance with `StatusAdapter` attached to it.
+fn setup_with_status_adapter(dir_name: &str, genesis: Block, adapter: Arc<StatusAdapter>) -> Chain {
+	util::init_test_logger();
+	clean_output_dir(dir_name);
+	let verifier_cache = Arc::new(RwLock::new(LruVerifierCache::new()));
+	let chain = chain::Chain::init(
+		dir_name.to_string(),
+		adapter,
+		genesis,
+		pow::verify_size,
+		verifier_cache,
+		false,
+	)
+	.unwrap();
+
+	chain
 }
 
 #[test]
@@ -156,6 +193,78 @@ where
 
 		chain.validate(false).unwrap();
 	}
+}
+
+#[test]
+// This test creates a reorg at REORG_DEPTH by mining a block with difficulty that
+// exceeds original chain total difficulty.
+//
+// Illustration of reorg with NUM_BLOCKS_MAIN = 6 and REORG_DEPTH = 5:
+//
+// difficulty:    1        2        3        4        5        6
+//
+//                       / [ 2  ] - [ 3  ] - [ 4  ] - [ 5  ] - [ 6  ] <- original chain
+// [ Genesis ] -[ 1 ]- *
+//                     ^ \ [ 2' ] - ................................  <- reorg chain with depth 5
+//                     |
+// difficulty:    1    |   24
+//                     |
+//                     \----< Fork point and chain reorg
+fn mine_reorg() {
+	// Test configuration
+	const NUM_BLOCKS_MAIN: u64 = 6; // Number of blocks to mine in main chain
+	const REORG_DEPTH: u64 = 5; // Number of blocks to be discarded from main chain after reorg
+
+	const DIR_NAME: &str = ".grin_reorg";
+	clean_output_dir(DIR_NAME);
+
+	global::set_mining_mode(ChainTypes::AutomatedTesting);
+	let kc = ExtKeychain::from_random_seed(false).unwrap();
+
+	let genesis = pow::mine_genesis_block().unwrap();
+	{
+		// Create chain that reports last block status
+		let mut last_status = RwLock::new(None);
+		let adapter = Arc::new(StatusAdapter::new(last_status));
+		let chain = setup_with_status_adapter(DIR_NAME, genesis.clone(), adapter.clone());
+
+		// Add blocks to main chain with gradually increasing difficulty
+		let mut prev = chain.head_header().unwrap();
+		for n in 1..=NUM_BLOCKS_MAIN {
+			let b = prepare_block(&kc, &prev, &chain, n);
+			prev = b.header.clone();
+			chain.process_block(b, chain::Options::SKIP_POW).unwrap();
+		}
+
+		let head = chain.head_header().unwrap();
+		assert_eq!(head.height, NUM_BLOCKS_MAIN);
+		assert_eq!(head.hash(), prev.hash());
+
+		// Reorg chain should exceed main chain's total difficulty to be considered
+		let reorg_difficulty = head.total_difficulty().to_num();
+
+		// Create one block for reorg chain forking off NUM_BLOCKS_MAIN - REORG_DEPTH height
+		let fork_head = chain
+			.get_header_by_height(NUM_BLOCKS_MAIN - REORG_DEPTH)
+			.unwrap();
+		let b = prepare_fork_block(&kc, &fork_head, &chain, reorg_difficulty);
+		let reorg_head = b.header.clone();
+		chain.process_block(b, chain::Options::SKIP_POW).unwrap();
+
+		// Check that reorg is correctly reported in block status
+		assert_eq!(
+			*adapter.last_status.read(),
+			Some(BlockStatus::Reorg(REORG_DEPTH))
+		);
+
+		// Chain should be switched to the reorganized chain
+		let head = chain.head_header().unwrap();
+		assert_eq!(head.height, NUM_BLOCKS_MAIN - REORG_DEPTH + 1);
+		assert_eq!(head.hash(), reorg_head.hash());
+	}
+
+	// Cleanup chain directory
+	clean_output_dir(DIR_NAME);
 }
 
 #[test]

keplernetwork/kepler

chain/tests/mine_simple_chain.rs
@@ -26,9 +26,11 @@ use self::keychain::{ExtKeychain, ExtKeychainPath, Keychain};
 use self::util::{RwLock, StopState};
 use chrono::Duration;
 use kepler_chain as chain;
+use kepler_chain::{BlockStatus, ChainAdapter, Options};
 use kepler_core as core;
 use kepler_keychain as keychain;
 use kepler_util as util;
+use std::cell::RefCell;
 use std::fs;
 use std::sync::Arc;
 
@@ -49,6 +51,41 @@ fn setup(dir_name: &str, genesis: Block) -> Chain {
 		false,
 	)
 	.unwrap()
+}
+
+/// Adapter to retrieve last status
+pub struct StatusAdapter {
+	pub last_status: RwLock<Option<BlockStatus>>,
+}
+
+impl StatusAdapter {
+	pub fn new(last_status: RwLock<Option<BlockStatus>>) -> Self {
+		StatusAdapter { last_status }
+	}
+}
+
+impl ChainAdapter for StatusAdapter {
+	fn block_accepted(&self, _b: &Block, status: BlockStatus, _opts: Options) {
+		*self.last_status.write() = Some(status);
+	}
+}
+
+/// Creates a `Chain` instance with `StatusAdapter` attached to it.
+fn setup_with_status_adapter(dir_name: &str, genesis: Block, adapter: Arc<StatusAdapter>) -> Chain {
+	util::init_test_logger();
+	clean_output_dir(dir_name);
+	let verifier_cache = Arc::new(RwLock::new(LruVerifierCache::new()));
+	let chain = chain::Chain::init(
+		dir_name.to_string(),
+		adapter,
+		genesis,
+		pow::verify_size,
+		verifier_cache,
+		false,
+	)
+	.unwrap();
+
+	chain
 }
 
 #[test]
@@ -156,6 +193,78 @@ where
 
 		chain.validate(false).unwrap();
 	}
+}
+
+#[test]
+// This test creates a reorg at REORG_DEPTH by mining a block with difficulty that
+// exceeds original chain total difficulty.
+//
+// Illustration of reorg with NUM_BLOCKS_MAIN = 6 and REORG_DEPTH = 5:
+//
+// difficulty:    1        2        3        4        5        6
+//
+//                       / [ 2  ] - [ 3  ] - [ 4  ] - [ 5  ] - [ 6  ] <- original chain
+// [ Genesis ] -[ 1 ]- *
+//                     ^ \ [ 2' ] - ................................  <- reorg chain with depth 5
+//                     |
+// difficulty:    1    |   24
+//                     |
+//                     \----< Fork point and chain reorg
+fn mine_reorg() {
+	// Test configuration
+	const NUM_BLOCKS_MAIN: u64 = 6; // Number of blocks to mine in main chain
+	const REORG_DEPTH: u64 = 5; // Number of blocks to be discarded from main chain after reorg
+
+	const DIR_NAME: &str = ".grin_reorg";
+	clean_output_dir(DIR_NAME);
+
+	global::set_mining_mode(ChainTypes::AutomatedTesting);
+	let kc = ExtKeychain::from_random_seed(false).unwrap();
+
+	let genesis = pow::mine_genesis_block().unwrap();
+	{
+		// Create chain that reports last block status
+		let mut last_status = RwLock::new(None);
+		let adapter = Arc::new(StatusAdapter::new(last_status));
+		let chain = setup_with_status_adapter(DIR_NAME, genesis.clone(), adapter.clone());
+
+		// Add blocks to main chain with gradually increasing difficulty
+		let mut prev = chain.head_header().unwrap();
+		for n in 1..=NUM_BLOCKS_MAIN {
+			let b = prepare_block(&kc, &prev, &chain, n);
+			prev = b.header.clone();
+			chain.process_block(b, chain::Options::SKIP_POW).unwrap();
+		}
+
+		let head = chain.head_header().unwrap();
+		assert_eq!(head.height, NUM_BLOCKS_MAIN);
+		assert_eq!(head.hash(), prev.hash());
+
+		// Reorg chain should exceed main chain's total difficulty to be considered
+		let reorg_difficulty = head.total_difficulty().to_num();
+
+		// Create one block for reorg chain forking off NUM_BLOCKS_MAIN - REORG_DEPTH height
+		let fork_head = chain
+			.get_header_by_height(NUM_BLOCKS_MAIN - REORG_DEPTH)
+			.unwrap();
+		let b = prepare_fork_block(&kc, &fork_head, &chain, reorg_difficulty);
+		let reorg_head = b.header.clone();
+		chain.process_block(b, chain::Options::SKIP_POW).unwrap();
+
+		// Check that reorg is correctly reported in block status
+		assert_eq!(
+			*adapter.last_status.read(),
+			Some(BlockStatus::Reorg(REORG_DEPTH))
+		);
+
+		// Chain should be switched to the reorganized chain
+		let head = chain.head_header().unwrap();
+		assert_eq!(head.height, NUM_BLOCKS_MAIN - REORG_DEPTH + 1);
+		assert_eq!(head.hash(), reorg_head.hash());
+	}
+
+	// Cleanup chain directory
+	clean_output_dir(DIR_NAME);
 }
 
 #[test]