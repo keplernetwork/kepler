mwcproject/mwc-node

Support new Bulletproof rewind scheme (#2848)
* Update keychain with new rewind scheme

* Refactor: proof builder trait

* Update tests, cleanup

* rustfmt

* Move conversion of SwitchCommitmentType

* Add proof build trait to tx builders

* Cache hashes in proof builders

* Proof builder tests

* Add ViewKey struct

* Fix some warnings

* Zeroize proof builder secrets on drop

core/src/libtx/reward.rs
@@ -19,25 +19,33 @@ use crate::core::transaction::kernel_sig_msg;
 use crate::core::{KernelFeatures, Output, OutputFeatures, TxKernel};
 use crate::keychain::{Identifier, Keychain};
 use crate::libtx::error::Error;
-use crate::libtx::{aggsig, proof};
+use crate::libtx::{
+	aggsig,
+	proof::{self, ProofBuild},
+};
 use crate::util::{secp, static_secp_instance};
+use grin_keychain::SwitchCommitmentType;
 
 /// output a reward output
-pub fn output<K>(
+pub fn output<K, B>(
 	keychain: &K,
+	builder: &B,
 	key_id: &Identifier,
 	fees: u64,
 	test_mode: bool,
 ) -> Result<(Output, TxKernel), Error>
 where
 	K: Keychain,
+	B: ProofBuild,
 {
 	let value = reward(fees);
-	let commit = keychain.commit(value, key_id)?;
+	// TODO: proper support for different switch commitment schemes
+	let switch = &SwitchCommitmentType::Regular;
+	let commit = keychain.commit(value, key_id, switch)?;
 
 	trace!("Block reward - Pedersen Commit is: {:?}", commit,);
 
-	let rproof = proof::create(keychain, value, key_id, commit, None)?;
+	let rproof = proof::create(keychain, builder, value, key_id, switch, commit, None)?;
 
 	let output = Output {
 		features: OutputFeatures::Coinbase,

keychain/src/keychain.rs
@@ -17,20 +17,31 @@
 use rand::distributions::Alphanumeric;
 use rand::{thread_rng, Rng};
 
-use crate::blake2;
+use crate::blake2::blake2b::blake2b;
 
-use crate::extkey_bip32::{BIP32GrinHasher, ExtendedPrivKey};
-use crate::types::{BlindSum, BlindingFactor, Error, ExtKeychainPath, Identifier, Keychain};
-use crate::util::secp::key::SecretKey;
+use crate::extkey_bip32::{BIP32GrinHasher, ExtendedPrivKey, ExtendedPubKey};
+use crate::types::{
+	BlindSum, BlindingFactor, Error, ExtKeychainPath, Identifier, Keychain, SwitchCommitmentType,
+};
+use crate::util::secp::key::{PublicKey, SecretKey};
 use crate::util::secp::pedersen::Commitment;
 use crate::util::secp::{self, Message, Secp256k1, Signature};
 
 #[derive(Clone, Debug)]
 pub struct ExtKeychain {
 	secp: Secp256k1,
-	master: ExtendedPrivKey,
-	use_switch_commits: bool,
+	pub master: ExtendedPrivKey,
 	hasher: BIP32GrinHasher,
+}
+
+impl ExtKeychain {
+	pub fn pub_root_key(&mut self) -> ExtendedPubKey {
+		ExtendedPubKey::from_private(&self.secp, &self.master, &mut self.hasher)
+	}
+
+	pub fn hasher(&self) -> BIP32GrinHasher {
+		self.hasher.clone()
+	}
 }
 
 impl Keychain for ExtKeychain {
@@ -41,7 +52,6 @@ impl Keychain for ExtKeychain {
 		let keychain = ExtKeychain {
 			secp: secp,
 			master: master,
-			use_switch_commits: true,
 			hasher: h,
 		};
 		Ok(keychain)
@@ -54,7 +64,6 @@ impl Keychain for ExtKeychain {
 		let keychain = ExtKeychain {
 			secp: secp,
 			master: master,
-			use_switch_commits: true,
 			hasher: h,
 		};
 		Ok(keychain)
@@ -63,7 +72,7 @@ impl Keychain for ExtKeychain {
 	/// For testing - probably not a good idea to use outside of tests.
 	fn from_random_seed(is_floo: bool) -> Result<ExtKeychain, Error> {
 		let seed: String = thread_rng().sample_iter(&Alphanumeric).take(16).collect();
-		let seed = blake2::blake2b::blake2b(32, &[], seed.as_bytes());
+		let seed = blake2b(32, &[], seed.as_bytes());
 		ExtKeychain::from_seed(seed.as_bytes(), is_floo)
 	}
 
@@ -75,22 +84,39 @@ impl Keychain for ExtKeychain {
 		ExtKeychainPath::new(depth, d1, d2, d3, d4).to_identifier()
 	}
 
-	fn derive_key(&self, amount: u64, id: &Identifier) -> Result<SecretKey, Error> {
+	fn public_root_key(&self) -> PublicKey {
+		let mut hasher = self.hasher.clone();
+		ExtendedPubKey::from_private(&self.secp, &self.master, &mut hasher).public_key
+	}
+
+	fn derive_key(
+		&self,
+		amount: u64,
+		id: &Identifier,
+		switch: &SwitchCommitmentType,
+	) -> Result<SecretKey, Error> {
 		let mut h = self.hasher.clone();
 		let p = id.to_path();
-		let mut ext_key = self.master;
+		let mut ext_key = self.master.clone();
 		for i in 0..p.depth {
 			ext_key = ext_key.ckd_priv(&self.secp, &mut h, p.path[i as usize])?;
 		}
 
-		match self.use_switch_commits {
-			true => Ok(self.secp.blind_switch(amount, ext_key.secret_key)?),
-			false => Ok(ext_key.secret_key),
+		match *switch {
+			SwitchCommitmentType::Regular => {
+				Ok(self.secp.blind_switch(amount, ext_key.secret_key)?)
+			}
+			SwitchCommitmentType::None => Ok(ext_key.secret_key),
 		}
 	}
 
-	fn commit(&self, amount: u64, id: &Identifier) -> Result<Commitment, Error> {
-		let key = self.derive_key(amount, id)?;
+	fn commit(
+		&self,
+		amount: u64,
+		id: &Identifier,
+		switch: &SwitchCommitmentType,
+	) -> Result<Commitment, Error> {
+		let key = self.derive_key(amount, id, switch)?;
 		let commit = self.secp.commit(amount, key)?;
 		Ok(commit)
 	}
@@ -100,7 +126,11 @@ impl Keychain for ExtKeychain {
 			.positive_key_ids
 			.iter()
 			.filter_map(|k| {
-				let res = self.derive_key(k.value, &Identifier::from_path(&k.ext_keychain_path));
+				let res = self.derive_key(
+					k.value,
+					&Identifier::from_path(&k.ext_keychain_path),
+					&k.switch,
+				);
 				if let Ok(s) = res {
 					Some(s)
 				} else {
@@ -113,7 +143,11 @@ impl Keychain for ExtKeychain {
 			.negative_key_ids
 			.iter()
 			.filter_map(|k| {
-				let res = self.derive_key(k.value, &Identifier::from_path(&k.ext_keychain_path));
+				let res = self.derive_key(
+					k.value,
+					&Identifier::from_path(&k.ext_keychain_path),
+					&k.switch,
+				);
 				if let Ok(s) = res {
 					Some(s)
 				} else {
@@ -122,37 +156,32 @@ impl Keychain for ExtKeychain {
 			})
 			.collect();
 
-		pos_keys.extend(
-			&blind_sum
-				.positive_blinding_factors
-				.iter()
-				.filter_map(|b| b.secret_key(&self.secp).ok())
-				.collect::<Vec<SecretKey>>(),
-		);
+		let keys = blind_sum
+			.positive_blinding_factors
+			.iter()
+			.filter_map(|b| b.secret_key(&self.secp).ok().clone())
+			.collect::<Vec<SecretKey>>();
+		pos_keys.extend(keys);
 
-		neg_keys.extend(
-			&blind_sum
-				.negative_blinding_factors
-				.iter()
-				.filter_map(|b| b.secret_key(&self.secp).ok())
-				.collect::<Vec<SecretKey>>(),
-		);
+		let keys = blind_sum
+			.negative_blinding_factors
+			.iter()
+			.filter_map(|b| b.secret_key(&self.secp).ok().clone())
+			.collect::<Vec<SecretKey>>();
+		neg_keys.extend(keys);
 
 		let sum = self.secp.blind_sum(pos_keys, neg_keys)?;
 		Ok(BlindingFactor::from_secret_key(sum))
 	}
 
-	fn create_nonce(&self, commit: &Commitment) -> Result<SecretKey, Error> {
-		// hash(commit|wallet root secret key (m)) as nonce
-		let root_key = self.derive_key(0, &Self::root_key_id())?;
-		let res = blake2::blake2b::blake2b(32, &commit.0, &root_key.0[..]);
-		let res = res.as_bytes();
-		SecretKey::from_slice(&self.secp, &res)
-			.map_err(|e| Error::RangeProof(format!("Unable to create nonce: {:?}", e).to_string()))
-	}
-
-	fn sign(&self, msg: &Message, amount: u64, id: &Identifier) -> Result<Signature, Error> {
-		let skey = self.derive_key(amount, id)?;
+	fn sign(
+		&self,
+		msg: &Message,
+		amount: u64,
+		id: &Identifier,
+		switch: &SwitchCommitmentType,
+	) -> Result<Signature, Error> {
+		let skey = self.derive_key(amount, id, switch)?;
 		let sig = self.secp.sign(msg, &skey)?;
 		Ok(sig)
 	}
@@ -167,10 +196,6 @@ impl Keychain for ExtKeychain {
 		Ok(sig)
 	}
 
-	fn set_use_switch_commits(&mut self, value: bool) {
-		self.use_switch_commits = value;
-	}
-
 	fn secp(&self) -> &Secp256k1 {
 		&self.secp
 	}
@@ -182,11 +207,13 @@ mod test {
 	use crate::types::{BlindSum, BlindingFactor, ExtKeychainPath, Keychain};
 	use crate::util::secp;
 	use crate::util::secp::key::SecretKey;
+	use crate::SwitchCommitmentType;
 
 	#[test]
 	fn test_key_derivation() {
 		let keychain = ExtKeychain::from_random_seed(false).unwrap();
 		let secp = keychain.secp();
+		let switch = &SwitchCommitmentType::None;
 
 		let path = ExtKeychainPath::new(1, 1, 0, 0, 0);
 		let key_id = path.to_identifier();
@@ -196,10 +223,10 @@ mod test {
 
 		// now create a zero commitment using the key on the keychain associated with
 		// the key_id
-		let commit = keychain.commit(0, &key_id).unwrap();
+		let commit = keychain.commit(0, &key_id, switch).unwrap();
 
 		// now check we can use our key to verify a signature from this zero commitment
-		let sig = keychain.sign(&msg, 0, &key_id).unwrap();
+		let sig = keychain.sign(&msg, 0, &key_id, switch).unwrap();
 		secp.verify_from_commit(&msg, &sig, &commit).unwrap();
 	}
 
@@ -235,9 +262,9 @@ mod test {
 
 		// create commitments for secret keys 1, 2 and 3
 		// all committing to the value 0 (which is what we do for tx_kernels)
-		let commit_1 = keychain.secp.commit(0, skey1).unwrap();
-		let commit_2 = keychain.secp.commit(0, skey2).unwrap();
-		let commit_3 = keychain.secp.commit(0, skey3).unwrap();
+		let commit_1 = keychain.secp.commit(0, skey1.clone()).unwrap();
+		let commit_2 = keychain.secp.commit(0, skey2.clone()).unwrap();
+		let commit_3 = keychain.secp.commit(0, skey3.clone()).unwrap();
 
 		// now sum commitments for keys 1 and 2
 		let sum = keychain

keplernetwork/kepler

core/src/libtx/reward.rs
@@ -19,12 +19,17 @@ use crate::core::transaction::kernel_sig_msg;
 use crate::core::{KernelFeatures, Output, OutputFeatures, TxKernel};
 use crate::keychain::{Identifier, Keychain};
 use crate::libtx::error::Error;
-use crate::libtx::{aggsig, proof};
+use crate::libtx::{
+	aggsig,
+	proof::{self, ProofBuild},
+};
 use crate::util::{secp, static_secp_instance};
+use kepler_keychain::SwitchCommitmentType;
 
 /// output a reward output
-pub fn output<K>(
+pub fn output<K, B>(
 	keychain: &K,
+	builder: &B,
 	key_id: &Identifier,
 	fees: u64,
 	height: u64,
@@ -32,13 +37,16 @@ pub fn output<K>(
 ) -> Result<(Output, TxKernel), Error>
 where
 	K: Keychain,
+	B: ProofBuild,
 {
 	let value = reward(height, fees);
-	let commit = keychain.commit(value, key_id)?;
+	// TODO: proper support for different switch commitment schemes
+	let switch = &SwitchCommitmentType::Regular;
+	let commit = keychain.commit(value, key_id, switch)?;
 
 	trace!("Block reward - Pedersen Commit is: {:?}", commit,);
 
-	let rproof = proof::create(keychain, value, key_id, commit, None)?;
+	let rproof = proof::create(keychain, builder, value, key_id, switch, commit, None)?;
 
 	let output = Output {
 		features: OutputFeatures::Coinbase,

keychain/src/keychain.rs
@@ -17,20 +17,31 @@
 use rand::distributions::Alphanumeric;
 use rand::{thread_rng, Rng};
 
-use crate::blake2;
+use crate::blake2::blake2b::blake2b;
 
-use crate::extkey_bip32::{BIP32KeplerHasher, ExtendedPrivKey};
-use crate::types::{BlindSum, BlindingFactor, Error, ExtKeychainPath, Identifier, Keychain};
-use crate::util::secp::key::SecretKey;
+use crate::extkey_bip32::{BIP32KeplerHasher, ExtendedPrivKey, ExtendedPubKey};
+use crate::types::{
+	BlindSum, BlindingFactor, Error, ExtKeychainPath, Identifier, Keychain, SwitchCommitmentType,
+};
+use crate::util::secp::key::{PublicKey, SecretKey};
 use crate::util::secp::pedersen::Commitment;
 use crate::util::secp::{self, Message, Secp256k1, Signature};
 
 #[derive(Clone, Debug)]
 pub struct ExtKeychain {
 	secp: Secp256k1,
-	master: ExtendedPrivKey,
-	use_switch_commits: bool,
+	pub master: ExtendedPrivKey,
 	hasher: BIP32KeplerHasher,
+}
+
+impl ExtKeychain {
+	pub fn pub_root_key(&mut self) -> ExtendedPubKey {
+		ExtendedPubKey::from_private(&self.secp, &self.master, &mut self.hasher)
+	}
+
+	pub fn hasher(&self) -> BIP32KeplerHasher {
+		self.hasher.clone()
+	}
 }
 
 impl Keychain for ExtKeychain {
@@ -41,7 +52,6 @@ impl Keychain for ExtKeychain {
 		let keychain = ExtKeychain {
 			secp: secp,
 			master: master,
-			use_switch_commits: true,
 			hasher: h,
 		};
 		Ok(keychain)
@@ -54,7 +64,6 @@ impl Keychain for ExtKeychain {
 		let keychain = ExtKeychain {
 			secp: secp,
 			master: master,
-			use_switch_commits: true,
 			hasher: h,
 		};
 		Ok(keychain)
@@ -63,7 +72,7 @@ impl Keychain for ExtKeychain {
 	/// For testing - probably not a good idea to use outside of tests.
 	fn from_random_seed(is_floo: bool) -> Result<ExtKeychain, Error> {
 		let seed: String = thread_rng().sample_iter(&Alphanumeric).take(16).collect();
-		let seed = blake2::blake2b::blake2b(32, &[], seed.as_bytes());
+		let seed = blake2b(32, &[], seed.as_bytes());
 		ExtKeychain::from_seed(seed.as_bytes(), is_floo)
 	}
 
@@ -75,22 +84,39 @@ impl Keychain for ExtKeychain {
 		ExtKeychainPath::new(depth, d1, d2, d3, d4).to_identifier()
 	}
 
-	fn derive_key(&self, amount: u64, id: &Identifier) -> Result<SecretKey, Error> {
+	fn public_root_key(&self) -> PublicKey {
+		let mut hasher = self.hasher.clone();
+		ExtendedPubKey::from_private(&self.secp, &self.master, &mut hasher).public_key
+	}
+
+	fn derive_key(
+		&self,
+		amount: u64,
+		id: &Identifier,
+		switch: &SwitchCommitmentType,
+	) -> Result<SecretKey, Error> {
 		let mut h = self.hasher.clone();
 		let p = id.to_path();
-		let mut ext_key = self.master;
+		let mut ext_key = self.master.clone();
 		for i in 0..p.depth {
 			ext_key = ext_key.ckd_priv(&self.secp, &mut h, p.path[i as usize])?;
 		}
 
-		match self.use_switch_commits {
-			true => Ok(self.secp.blind_switch(amount, ext_key.secret_key)?),
-			false => Ok(ext_key.secret_key),
+		match *switch {
+			SwitchCommitmentType::Regular => {
+				Ok(self.secp.blind_switch(amount, ext_key.secret_key)?)
+			}
+			SwitchCommitmentType::None => Ok(ext_key.secret_key),
 		}
 	}
 
-	fn commit(&self, amount: u64, id: &Identifier) -> Result<Commitment, Error> {
-		let key = self.derive_key(amount, id)?;
+	fn commit(
+		&self,
+		amount: u64,
+		id: &Identifier,
+		switch: &SwitchCommitmentType,
+	) -> Result<Commitment, Error> {
+		let key = self.derive_key(amount, id, switch)?;
 		let commit = self.secp.commit(amount, key)?;
 		Ok(commit)
 	}
@@ -100,7 +126,11 @@ impl Keychain for ExtKeychain {
 			.positive_key_ids
 			.iter()
 			.filter_map(|k| {
-				let res = self.derive_key(k.value, &Identifier::from_path(&k.ext_keychain_path));
+				let res = self.derive_key(
+					k.value,
+					&Identifier::from_path(&k.ext_keychain_path),
+					&k.switch,
+				);
 				if let Ok(s) = res {
 					Some(s)
 				} else {
@@ -113,7 +143,11 @@ impl Keychain for ExtKeychain {
 			.negative_key_ids
 			.iter()
 			.filter_map(|k| {
-				let res = self.derive_key(k.value, &Identifier::from_path(&k.ext_keychain_path));
+				let res = self.derive_key(
+					k.value,
+					&Identifier::from_path(&k.ext_keychain_path),
+					&k.switch,
+				);
 				if let Ok(s) = res {
 					Some(s)
 				} else {
@@ -122,37 +156,32 @@ impl Keychain for ExtKeychain {
 			})
 			.collect();
 
-		pos_keys.extend(
-			&blind_sum
-				.positive_blinding_factors
-				.iter()
-				.filter_map(|b| b.secret_key(&self.secp).ok())
-				.collect::<Vec<SecretKey>>(),
-		);
+		let keys = blind_sum
+			.positive_blinding_factors
+			.iter()
+			.filter_map(|b| b.secret_key(&self.secp).ok().clone())
+			.collect::<Vec<SecretKey>>();
+		pos_keys.extend(keys);
 
-		neg_keys.extend(
-			&blind_sum
-				.negative_blinding_factors
-				.iter()
-				.filter_map(|b| b.secret_key(&self.secp).ok())
-				.collect::<Vec<SecretKey>>(),
-		);
+		let keys = blind_sum
+			.negative_blinding_factors
+			.iter()
+			.filter_map(|b| b.secret_key(&self.secp).ok().clone())
+			.collect::<Vec<SecretKey>>();
+		neg_keys.extend(keys);
 
 		let sum = self.secp.blind_sum(pos_keys, neg_keys)?;
 		Ok(BlindingFactor::from_secret_key(sum))
 	}
 
-	fn create_nonce(&self, commit: &Commitment) -> Result<SecretKey, Error> {
-		// hash(commit|wallet root secret key (m)) as nonce
-		let root_key = self.derive_key(0, &Self::root_key_id())?;
-		let res = blake2::blake2b::blake2b(32, &commit.0, &root_key.0[..]);
-		let res = res.as_bytes();
-		SecretKey::from_slice(&self.secp, &res)
-			.map_err(|e| Error::RangeProof(format!("Unable to create nonce: {:?}", e).to_string()))
-	}
-
-	fn sign(&self, msg: &Message, amount: u64, id: &Identifier) -> Result<Signature, Error> {
-		let skey = self.derive_key(amount, id)?;
+	fn sign(
+		&self,
+		msg: &Message,
+		amount: u64,
+		id: &Identifier,
+		switch: &SwitchCommitmentType,
+	) -> Result<Signature, Error> {
+		let skey = self.derive_key(amount, id, switch)?;
 		let sig = self.secp.sign(msg, &skey)?;
 		Ok(sig)
 	}
@@ -167,10 +196,6 @@ impl Keychain for ExtKeychain {
 		Ok(sig)
 	}
 
-	fn set_use_switch_commits(&mut self, value: bool) {
-		self.use_switch_commits = value;
-	}
-
 	fn secp(&self) -> &Secp256k1 {
 		&self.secp
 	}
@@ -182,11 +207,13 @@ mod test {
 	use crate::types::{BlindSum, BlindingFactor, ExtKeychainPath, Keychain};
 	use crate::util::secp;
 	use crate::util::secp::key::SecretKey;
+	use crate::SwitchCommitmentType;
 
 	#[test]
 	fn test_key_derivation() {
 		let keychain = ExtKeychain::from_random_seed(false).unwrap();
 		let secp = keychain.secp();
+		let switch = &SwitchCommitmentType::None;
 
 		let path = ExtKeychainPath::new(1, 1, 0, 0, 0);
 		let key_id = path.to_identifier();
@@ -196,10 +223,10 @@ mod test {
 
 		// now create a zero commitment using the key on the keychain associated with
 		// the key_id
-		let commit = keychain.commit(0, &key_id).unwrap();
+		let commit = keychain.commit(0, &key_id, switch).unwrap();
 
 		// now check we can use our key to verify a signature from this zero commitment
-		let sig = keychain.sign(&msg, 0, &key_id).unwrap();
+		let sig = keychain.sign(&msg, 0, &key_id, switch).unwrap();
 		secp.verify_from_commit(&msg, &sig, &commit).unwrap();
 	}
 
@@ -235,9 +262,9 @@ mod test {
 
 		// create commitments for secret keys 1, 2 and 3
 		// all committing to the value 0 (which is what we do for tx_kernels)
-		let commit_1 = keychain.secp.commit(0, skey1).unwrap();
-		let commit_2 = keychain.secp.commit(0, skey2).unwrap();
-		let commit_3 = keychain.secp.commit(0, skey3).unwrap();
+		let commit_1 = keychain.secp.commit(0, skey1.clone()).unwrap();
+		let commit_2 = keychain.secp.commit(0, skey2.clone()).unwrap();
+		let commit_3 = keychain.secp.commit(0, skey3.clone()).unwrap();
 
 		// now sum commitments for keys 1 and 2
 		let sum = keychain