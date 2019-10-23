mwcproject/mwc-node

Rewind head and header_head consistently. (#2918)
* maintain header_head as distinctly separate from head

* cleanup corrupted storage log msg

* simplify process_header and check_header_known

* remember to commit the batch when successfully processing a header...

* rework sync_block_headers for consistency with process_block_header

* cleanup unrelated code

* fix pool tests

* cleanup chain tests

* cleanup chain tests (reuse helpers more)

* cleanup - head not header on an extension
shortcircuit "rewind and apply fork" for headers if next header

chain/tests/mine_simple_chain.rs
@@ -17,9 +17,8 @@ use self::chain::Chain;
 use self::core::core::hash::Hashed;
 use self::core::core::verifier_cache::LruVerifierCache;
 use self::core::core::{Block, BlockHeader, OutputIdentifier, Transaction};
-use self::core::genesis;
 use self::core::global::ChainTypes;
-use self::core::libtx::{self, build, reward, ProofBuilder};
+use self::core::libtx::{self, build, ProofBuilder};
 use self::core::pow::Difficulty;
 use self::core::{consensus, global, pow};
 use self::keychain::{ExtKeychain, ExtKeychainPath, Keychain};
@@ -30,27 +29,11 @@ use grin_chain::{BlockStatus, ChainAdapter, Options};
 use grin_core as core;
 use grin_keychain as keychain;
 use grin_util as util;
-use std::fs;
 use std::sync::Arc;
 
-fn clean_output_dir(dir_name: &str) {
-	let _ = fs::remove_dir_all(dir_name);
-}
+mod chain_test_helper;
 
-fn setup(dir_name: &str, genesis: Block) -> Chain {
-	util::init_test_logger();
-	clean_output_dir(dir_name);
-	let verifier_cache = Arc::new(RwLock::new(LruVerifierCache::new()));
-	chain::Chain::init(
-		dir_name.to_string(),
-		Arc::new(NoopAdapter {}),
-		genesis,
-		pow::verify_size,
-		verifier_cache,
-		false,
-	)
-	.unwrap()
-}
+use self::chain_test_helper::{clean_output_dir, init_chain, mine_chain};
 
 /// Adapter to retrieve last status
 pub struct StatusAdapter {
@@ -89,118 +72,20 @@ fn setup_with_status_adapter(dir_name: &str, genesis: Block, adapter: Arc<Status
 
 #[test]
 fn mine_empty_chain() {
-	global::set_mining_mode(ChainTypes::AutomatedTesting);
-	let keychain = keychain::ExtKeychain::from_random_seed(false).unwrap();
-	{
-		mine_some_on_top(".grin", pow::mine_genesis_block().unwrap(), &keychain);
-	}
-	// Cleanup chain directory
-	clean_output_dir(".grin");
+	let chain_dir = ".grin.empty";
+	clean_output_dir(chain_dir);
+	let chain = mine_chain(chain_dir, 1);
+	assert_eq!(chain.head().unwrap().height, 0);
+	clean_output_dir(chain_dir);
 }
 
 #[test]
-fn mine_genesis_reward_chain() {
-	global::set_mining_mode(ChainTypes::AutomatedTesting);
-
-	// add coinbase data from the dev genesis block
-	let mut genesis = genesis::genesis_dev();
-	let keychain = keychain::ExtKeychain::from_random_seed(false).unwrap();
-	let key_id = keychain::ExtKeychain::derive_key_id(0, 1, 0, 0, 0);
-	let reward = reward::output(
-		&keychain,
-		&libtx::ProofBuilder::new(&keychain),
-		&key_id,
-		0,
-		false,
-	)
-	.unwrap();
-	genesis = genesis.with_reward(reward.0, reward.1);
-
-	let tmp_chain_dir = ".grin.tmp";
-	{
-		// setup a tmp chain to hande tx hashsets
-		let tmp_chain = setup(tmp_chain_dir, pow::mine_genesis_block().unwrap());
-		tmp_chain.set_txhashset_roots(&mut genesis).unwrap();
-		genesis.header.output_mmr_size = 1;
-		genesis.header.kernel_mmr_size = 1;
-	}
-
-	// get a valid PoW
-	pow::pow_size(
-		&mut genesis.header,
-		Difficulty::unit(),
-		global::proofsize(),
-		global::min_edge_bits(),
-	)
-	.unwrap();
-
-	mine_some_on_top(".grin.genesis", genesis, &keychain);
-	// Cleanup chain directories
-	clean_output_dir(tmp_chain_dir);
-	clean_output_dir(".grin.genesis");
-}
-
-fn mine_some_on_top<K>(dir: &str, genesis: Block, keychain: &K)
-where
-	K: Keychain,
-{
-	let chain = setup(dir, genesis);
-
-	for n in 1..4 {
-		let prev = chain.head_header().unwrap();
-		let next_header_info = consensus::next_difficulty(1, chain.difficulty_iter().unwrap());
-		let pk = ExtKeychainPath::new(1, n as u32, 0, 0, 0).to_identifier();
-		let reward =
-			libtx::reward::output(keychain, &libtx::ProofBuilder::new(keychain), &pk, 0, false)
-				.unwrap();
-		let mut b =
-			core::core::Block::new(&prev, vec![], next_header_info.clone().difficulty, reward)
-				.unwrap();
-		b.header.timestamp = prev.timestamp + Duration::seconds(60);
-		b.header.pow.secondary_scaling = next_header_info.secondary_scaling;
-
-		chain.set_txhashset_roots(&mut b).unwrap();
-
-		let edge_bits = if n == 2 {
-			global::min_edge_bits() + 1
-		} else {
-			global::min_edge_bits()
-		};
-		b.header.pow.proof.edge_bits = edge_bits;
-		pow::pow_size(
-			&mut b.header,
-			next_header_info.difficulty,
-			global::proofsize(),
-			edge_bits,
-		)
-		.unwrap();
-		b.header.pow.proof.edge_bits = edge_bits;
-
-		let bhash = b.hash();
-		chain.process_block(b, chain::Options::MINE).unwrap();
-
-		// checking our new head
-		let head = chain.head().unwrap();
-		assert_eq!(head.height, n);
-		assert_eq!(head.last_block_h, bhash);
-
-		// now check the block_header of the head
-		let header = chain.head_header().unwrap();
-		assert_eq!(header.height, n);
-		assert_eq!(header.hash(), bhash);
-
-		// now check the block itself
-		let block = chain.get_block(&header.hash()).unwrap();
-		assert_eq!(block.header.height, n);
-		assert_eq!(block.hash(), bhash);
-		assert_eq!(block.outputs().len(), 1);
-
-		// now check the block height index
-		let header_by_height = chain.get_header_by_height(n).unwrap();
-		assert_eq!(header_by_height.hash(), bhash);
-
-		chain.validate(false).unwrap();
-	}
+fn mine_short_chain() {
+	let chain_dir = ".grin.genesis";
+	clean_output_dir(chain_dir);
+	let chain = mine_chain(chain_dir, 4);
+	assert_eq!(chain.head().unwrap().height, 3);
+	clean_output_dir(chain_dir);
 }
 
 #[test]
@@ -279,7 +164,7 @@ fn mine_reorg() {
 fn mine_forks() {
 	global::set_mining_mode(ChainTypes::AutomatedTesting);
 	{
-		let chain = setup(".grin2", pow::mine_genesis_block().unwrap());
+		let chain = init_chain(".grin2", pow::mine_genesis_block().unwrap());
 		let kc = ExtKeychain::from_random_seed(false).unwrap();
 
 		// add a first block to not fork genesis
@@ -294,9 +179,6 @@ fn mine_forks() {
 			let prev = chain.head_header().unwrap();
 			let b1 = prepare_block(&kc, &prev, &chain, 3 * n);
 
-			// 2nd block with higher difficulty for other branch
-			let b2 = prepare_block(&kc, &prev, &chain, 3 * n + 1);
-
 			// process the first block to extend the chain
 			let bhash = b1.hash();
 			chain.process_block(b1, chain::Options::SKIP_POW).unwrap();
@@ -306,6 +188,9 @@ fn mine_forks() {
 			assert_eq!(head.height, (n + 1) as u64);
 			assert_eq!(head.last_block_h, bhash);
 			assert_eq!(head.prev_block_h, prev.hash());
+
+			// 2nd block with higher difficulty for other branch
+			let b2 = prepare_block(&kc, &prev, &chain, 3 * n + 1);
 
 			// process the 2nd block to build a fork with more work
 			let bhash = b2.hash();
@@ -327,7 +212,7 @@ fn mine_losing_fork() {
 	global::set_mining_mode(ChainTypes::AutomatedTesting);
 	let kc = ExtKeychain::from_random_seed(false).unwrap();
 	{
-		let chain = setup(".grin3", pow::mine_genesis_block().unwrap());
+		let chain = init_chain(".grin3", pow::mine_genesis_block().unwrap());
 
 		// add a first block we'll be forking from
 		let prev = chain.head_header().unwrap();
@@ -367,7 +252,7 @@ fn longer_fork() {
 	// then send back on the 1st
 	let genesis = pow::mine_genesis_block().unwrap();
 	{
-		let chain = setup(".grin4", genesis.clone());
+		let chain = init_chain(".grin4", genesis.clone());
 
 		// add blocks to both chains, 20 on the main one, only the first 5
 		// for the forked chain
@@ -406,8 +291,11 @@ fn longer_fork() {
 fn spend_in_fork_and_compact() {
 	global::set_mining_mode(ChainTypes::AutomatedTesting);
 	util::init_test_logger();
+	// Cleanup chain directory
+	clean_output_dir(".grin6");
+
 	{
-		let chain = setup(".grin6", pow::mine_genesis_block().unwrap());
+		let chain = init_chain(".grin6", pow::mine_genesis_block().unwrap());
 		let prev = chain.head_header().unwrap();
 		let kc = ExtKeychain::from_random_seed(false).unwrap();
 		let pb = ProofBuilder::new(&kc);
@@ -541,7 +429,7 @@ fn spend_in_fork_and_compact() {
 fn output_header_mappings() {
 	global::set_mining_mode(ChainTypes::AutomatedTesting);
 	{
-		let chain = setup(
+		let chain = init_chain(
 			".grin_header_for_output",
 			pow::mine_genesis_block().unwrap(),
 		);
@@ -635,7 +523,7 @@ where
 	K: Keychain,
 {
 	let mut b = prepare_block_nosum(kc, prev, diff, vec![]);
-	chain.set_txhashset_roots_forked(&mut b, prev).unwrap();
+	chain.set_txhashset_roots(&mut b).unwrap();
 	b
 }
 
@@ -650,7 +538,7 @@ where
 	K: Keychain,
 {
 	let mut b = prepare_block_nosum(kc, prev, diff, txs);
-	chain.set_txhashset_roots_forked(&mut b, prev).unwrap();
+	chain.set_txhashset_roots(&mut b).unwrap();
 	b
 }
 

chain/tests/store_indices.rs
@@ -12,87 +12,46 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use self::chain::{Error, Tip};
 use self::core::core::hash::Hashed;
-use self::core::core::Block;
-use self::core::global::{self, ChainTypes};
-use self::core::libtx;
-use self::core::pow::{self, Difficulty};
-use self::keychain::{ExtKeychain, ExtKeychainPath, Keychain};
-use env_logger;
 use grin_chain as chain;
 use grin_core as core;
-use grin_keychain as keychain;
-use std::fs;
-use std::sync::Arc;
+use grin_util as util;
 
-fn clean_output_dir(dir_name: &str) {
-	let _ = fs::remove_dir_all(dir_name);
-}
+mod chain_test_helper;
 
-fn setup_chain(genesis: &Block, chain_store: Arc<chain::store::ChainStore>) -> Result<(), Error> {
-	let batch = chain_store.batch()?;
-	batch.save_block_header(&genesis.header)?;
-	batch.save_block(&genesis)?;
-	let head = Tip::from_header(&genesis.header);
-	batch.save_head(&head)?;
-	batch.save_block_header(&genesis.header)?;
-	batch.commit()?;
-	Ok(())
-}
+use self::chain_test_helper::{clean_output_dir, mine_chain};
 
 #[test]
-fn test_various_store_indices() {
-	match env_logger::try_init() {
-		Ok(_) => println!("Initializing env logger"),
-		Err(e) => println!("env logger already initialized: {:?}", e),
-	};
+fn test_store_indices() {
+	util::init_test_logger();
+
 	let chain_dir = ".grin_idx_1";
 	clean_output_dir(chain_dir);
 
-	let keychain = ExtKeychain::from_random_seed(false).unwrap();
-	let key_id = ExtKeychainPath::new(1, 1, 0, 0, 0).to_identifier();
+	let chain = mine_chain(chain_dir, 4);
 
-	let chain_store = Arc::new(chain::store::ChainStore::new(chain_dir).unwrap());
+	// Check head exists in the db.
+	assert_eq!(chain.head().unwrap().height, 3);
 
-	global::set_mining_mode(ChainTypes::AutomatedTesting);
-	let genesis = pow::mine_genesis_block().unwrap();
+	// Check the header exists in the db.
+	assert_eq!(chain.head_header().unwrap().height, 3);
 
-	setup_chain(&genesis, chain_store.clone()).unwrap();
+	// Check header_by_height index.
+	let block_header = chain.get_header_by_height(3).unwrap();
+	let block_hash = block_header.hash();
+	assert_eq!(block_hash, chain.head().unwrap().last_block_h);
 
-	let reward = libtx::reward::output(
-		&keychain,
-		&libtx::ProofBuilder::new(&keychain),
-		&key_id,
-		0,
-		false,
-	)
-	.unwrap();
-	let block = Block::new(&genesis.header, vec![], Difficulty::min(), reward).unwrap();
-	let block_hash = block.hash();
-
-	{
-		let batch = chain_store.batch().unwrap();
-		batch.save_block_header(&block.header).unwrap();
-		batch.save_block(&block).unwrap();
-		batch.commit().unwrap();
-	}
-
-	let block_header = chain_store.get_block_header(&block_hash).unwrap();
-	assert_eq!(block_header.hash(), block_hash);
-
-	// Test we can retrive the block from the db and that we can safely delete the
-	// block from the db even though the block_sums are missing.
 	{
 		// Block exists in the db.
-		assert!(chain_store.get_block(&block_hash).is_ok());
+		assert_eq!(chain.get_block(&block_hash).unwrap().hash(), block_hash);
 
-		// Block sums do not exist (we never set them up).
-		assert!(chain_store.get_block_sums(&block_hash).is_err());
+		// Check we have block_sums in the db.
+		assert!(chain.get_block_sums(&block_hash).is_ok());
 
 		{
 			// Start a new batch and delete the block.
-			let batch = chain_store.batch().unwrap();
+			let store = chain.store();
+			let batch = store.batch().unwrap();
 			assert!(batch.delete_block(&block_hash).is_ok());
 
 			// Block is deleted within this batch.
@@ -100,8 +59,9 @@ fn test_various_store_indices() {
 		}
 
 		// Check the batch did not commit any changes to the store .
-		assert!(chain_store.get_block(&block_hash).is_ok());
+		assert!(chain.get_block(&block_hash).is_ok());
 	}
+
 	// Cleanup chain directory
 	clean_output_dir(chain_dir);
 }

keplernetwork/kepler

chain/tests/mine_simple_chain.rs
@@ -17,9 +17,8 @@ use self::chain::Chain;
 use self::core::core::hash::Hashed;
 use self::core::core::verifier_cache::LruVerifierCache;
 use self::core::core::{Block, BlockHeader, OutputIdentifier, Transaction};
-use self::core::genesis;
 use self::core::global::ChainTypes;
-use self::core::libtx::{self, build, reward, ProofBuilder};
+use self::core::libtx::{self, build, ProofBuilder};
 use self::core::pow::Difficulty;
 use self::core::{consensus, global, pow};
 use self::keychain::{ExtKeychain, ExtKeychainPath, Keychain};
@@ -30,27 +29,11 @@ use kepler_chain::{BlockStatus, ChainAdapter, Options};
 use kepler_core as core;
 use kepler_keychain as keychain;
 use kepler_util as util;
-use std::fs;
 use std::sync::Arc;
 
-fn clean_output_dir(dir_name: &str) {
-	let _ = fs::remove_dir_all(dir_name);
-}
+mod chain_test_helper;
 
-fn setup(dir_name: &str, genesis: Block) -> Chain {
-	util::init_test_logger();
-	clean_output_dir(dir_name);
-	let verifier_cache = Arc::new(RwLock::new(LruVerifierCache::new()));
-	chain::Chain::init(
-		dir_name.to_string(),
-		Arc::new(NoopAdapter {}),
-		genesis,
-		pow::verify_size,
-		verifier_cache,
-		false,
-	)
-	.unwrap()
-}
+use self::chain_test_helper::{clean_output_dir, init_chain, mine_chain};
 
 /// Adapter to retrieve last status
 pub struct StatusAdapter {
@@ -89,125 +72,20 @@ fn setup_with_status_adapter(dir_name: &str, genesis: Block, adapter: Arc<Status
 
 #[test]
 fn mine_empty_chain() {
-	global::set_mining_mode(ChainTypes::AutomatedTesting);
-	let keychain = keychain::ExtKeychain::from_random_seed(false).unwrap();
-	{
-		mine_some_on_top(".kepler", pow::mine_genesis_block().unwrap(), &keychain);
-	}
-	// Cleanup chain directory
-	clean_output_dir(".kepler");
+	let chain_dir = ".kepler.empty";
+	clean_output_dir(chain_dir);
+	let chain = mine_chain(chain_dir, 1);
+	assert_eq!(chain.head().unwrap().height, 0);
+	clean_output_dir(chain_dir);
 }
 
 #[test]
-fn mine_genesis_reward_chain() {
-	global::set_mining_mode(ChainTypes::AutomatedTesting);
-
-	// add coinbase data from the dev genesis block
-	let mut genesis = genesis::genesis_dev();
-	let keychain = keychain::ExtKeychain::from_random_seed(false).unwrap();
-	let key_id = keychain::ExtKeychain::derive_key_id(0, 1, 0, 0, 0);
-	let reward = reward::output(
-		&keychain,
-		&libtx::ProofBuilder::new(&keychain),
-		&key_id,
-		0,
-		0,
-		false,
-	)
-	.unwrap();
-	genesis = genesis.with_reward(reward.0, reward.1);
-
-	let tmp_chain_dir = ".kepler.tmp";
-	{
-		// setup a tmp chain to hande tx hashsets
-		let tmp_chain = setup(tmp_chain_dir, pow::mine_genesis_block().unwrap());
-		tmp_chain.set_txhashset_roots(&mut genesis).unwrap();
-		genesis.header.output_mmr_size = 1;
-		genesis.header.kernel_mmr_size = 1;
-	}
-
-	// get a valid PoW
-	pow::pow_size(
-		&mut genesis.header,
-		Difficulty::unit(),
-		global::proofsize(),
-		global::min_edge_bits(),
-	)
-	.unwrap();
-
-	mine_some_on_top(".kepler.genesis", genesis, &keychain);
-	// Cleanup chain directories
-	clean_output_dir(tmp_chain_dir);
-	clean_output_dir(".kepler.genesis");
-}
-
-fn mine_some_on_top<K>(dir: &str, genesis: Block, keychain: &K)
-where
-	K: Keychain,
-{
-	let chain = setup(dir, genesis);
-
-	for n in 1..4 {
-		let prev = chain.head_header().unwrap();
-		let next_header_info = consensus::next_difficulty(1, chain.difficulty_iter().unwrap());
-		let pk = ExtKeychainPath::new(1, n as u32, 0, 0, 0).to_identifier();
-		let reward = libtx::reward::output(
-			keychain,
-			&libtx::ProofBuilder::new(keychain),
-			&pk,
-			0,
-			n,
-			false,
-		)
-		.unwrap();
-		let mut b =
-			core::core::Block::new(&prev, vec![], next_header_info.clone().difficulty, reward)
-				.unwrap();
-		b.header.timestamp = prev.timestamp + Duration::seconds(60);
-		b.header.pow.secondary_scaling = next_header_info.secondary_scaling;
-
-		chain.set_txhashset_roots(&mut b).unwrap();
-
-		let edge_bits = if n == 2 {
-			global::min_edge_bits() + 1
-		} else {
-			global::min_edge_bits()
-		};
-		b.header.pow.proof.edge_bits = edge_bits;
-		pow::pow_size(
-			&mut b.header,
-			next_header_info.difficulty,
-			global::proofsize(),
-			edge_bits,
-		)
-		.unwrap();
-		b.header.pow.proof.edge_bits = edge_bits;
-
-		let bhash = b.hash();
-		chain.process_block(b, chain::Options::MINE).unwrap();
-
-		// checking our new head
-		let head = chain.head().unwrap();
-		assert_eq!(head.height, n);
-		assert_eq!(head.last_block_h, bhash);
-
-		// now check the block_header of the head
-		let header = chain.head_header().unwrap();
-		assert_eq!(header.height, n);
-		assert_eq!(header.hash(), bhash);
-
-		// now check the block itself
-		let block = chain.get_block(&header.hash()).unwrap();
-		assert_eq!(block.header.height, n);
-		assert_eq!(block.hash(), bhash);
-		assert_eq!(block.outputs().len(), 1);
-
-		// now check the block height index
-		let header_by_height = chain.get_header_by_height(n).unwrap();
-		assert_eq!(header_by_height.hash(), bhash);
-
-		chain.validate(false).unwrap();
-	}
+fn mine_short_chain() {
+	let chain_dir = ".kepler.genesis";
+	clean_output_dir(chain_dir);
+	let chain = mine_chain(chain_dir, 4);
+	assert_eq!(chain.head().unwrap().height, 3);
+	clean_output_dir(chain_dir);
 }
 
 #[test]
@@ -286,7 +164,7 @@ fn mine_reorg() {
 fn mine_forks() {
 	global::set_mining_mode(ChainTypes::AutomatedTesting);
 	{
-		let chain = setup(".kepler2", pow::mine_genesis_block().unwrap());
+		let chain = init_chain(".kepler2", pow::mine_genesis_block().unwrap());
 		let kc = ExtKeychain::from_random_seed(false).unwrap();
 
 		// add a first block to not fork genesis
@@ -301,9 +179,6 @@ fn mine_forks() {
 			let prev = chain.head_header().unwrap();
 			let b1 = prepare_block(&kc, &prev, &chain, 3 * n);
 
-			// 2nd block with higher difficulty for other branch
-			let b2 = prepare_block(&kc, &prev, &chain, 3 * n + 1);
-
 			// process the first block to extend the chain
 			let bhash = b1.hash();
 			chain.process_block(b1, chain::Options::SKIP_POW).unwrap();
@@ -313,6 +188,9 @@ fn mine_forks() {
 			assert_eq!(head.height, (n + 1) as u64);
 			assert_eq!(head.last_block_h, bhash);
 			assert_eq!(head.prev_block_h, prev.hash());
+
+			// 2nd block with higher difficulty for other branch
+			let b2 = prepare_block(&kc, &prev, &chain, 3 * n + 1);
 
 			// process the 2nd block to build a fork with more work
 			let bhash = b2.hash();
@@ -334,7 +212,7 @@ fn mine_losing_fork() {
 	global::set_mining_mode(ChainTypes::AutomatedTesting);
 	let kc = ExtKeychain::from_random_seed(false).unwrap();
 	{
-		let chain = setup(".kepler3", pow::mine_genesis_block().unwrap());
+		let chain = init_chain(".kepler3", pow::mine_genesis_block().unwrap());
 
 		// add a first block we'll be forking from
 		let prev = chain.head_header().unwrap();
@@ -374,7 +252,7 @@ fn longer_fork() {
 	// then send back on the 1st
 	let genesis = pow::mine_genesis_block().unwrap();
 	{
-		let chain = setup(".kepler4", genesis.clone());
+		let chain = init_chain(".kepler4", genesis.clone());
 
 		// add blocks to both chains, 20 on the main one, only the first 5
 		// for the forked chain
@@ -413,8 +291,11 @@ fn longer_fork() {
 fn spend_in_fork_and_compact() {
 	global::set_mining_mode(ChainTypes::AutomatedTesting);
 	util::init_test_logger();
+	// Cleanup chain directory
+	clean_output_dir(".kepler6");
+
 	{
-		let chain = setup(".kepler6", pow::mine_genesis_block().unwrap());
+		let chain = init_chain(".kepler6", pow::mine_genesis_block().unwrap());
 		let prev = chain.head_header().unwrap();
 		let kc = ExtKeychain::from_random_seed(false).unwrap();
 		let pb = ProofBuilder::new(&kc);
@@ -557,7 +438,7 @@ fn spend_in_fork_and_compact() {
 fn output_header_mappings() {
 	global::set_mining_mode(ChainTypes::AutomatedTesting);
 	{
-		let chain = setup(
+		let chain = init_chain(
 			".kepler_header_for_output",
 			pow::mine_genesis_block().unwrap(),
 		);
@@ -653,7 +534,7 @@ where
 	K: Keychain,
 {
 	let mut b = prepare_block_nosum(kc, prev, diff, vec![]);
-	chain.set_txhashset_roots_forked(&mut b, prev).unwrap();
+	chain.set_txhashset_roots(&mut b).unwrap();
 	b
 }
 
@@ -668,7 +549,7 @@ where
 	K: Keychain,
 {
 	let mut b = prepare_block_nosum(kc, prev, diff, txs);
-	chain.set_txhashset_roots_forked(&mut b, prev).unwrap();
+	chain.set_txhashset_roots(&mut b).unwrap();
 	b
 }
 

chain/tests/store_indices.rs
@@ -12,88 +12,46 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use self::chain::{Error, Tip};
 use self::core::core::hash::Hashed;
-use self::core::core::Block;
-use self::core::global::{self, ChainTypes};
-use self::core::libtx;
-use self::core::pow::{self, Difficulty};
-use self::keychain::{ExtKeychain, ExtKeychainPath, Keychain};
-use env_logger;
 use kepler_chain as chain;
 use kepler_core as core;
-use kepler_keychain as keychain;
-use std::fs;
-use std::sync::Arc;
+use kepler_util as util;
 
-fn clean_output_dir(dir_name: &str) {
-	let _ = fs::remove_dir_all(dir_name);
-}
+mod chain_test_helper;
 
-fn setup_chain(genesis: &Block, chain_store: Arc<chain::store::ChainStore>) -> Result<(), Error> {
-	let batch = chain_store.batch()?;
-	batch.save_block_header(&genesis.header)?;
-	batch.save_block(&genesis)?;
-	let head = Tip::from_header(&genesis.header);
-	batch.save_head(&head)?;
-	batch.save_block_header(&genesis.header)?;
-	batch.commit()?;
-	Ok(())
-}
+use self::chain_test_helper::{clean_output_dir, mine_chain};
 
 #[test]
-fn test_various_store_indices() {
-	match env_logger::try_init() {
-		Ok(_) => println!("Initializing env logger"),
-		Err(e) => println!("env logger already initialized: {:?}", e),
-	};
+fn test_store_indices() {
+	util::init_test_logger();
+
 	let chain_dir = ".kepler_idx_1";
 	clean_output_dir(chain_dir);
 
-	let keychain = ExtKeychain::from_random_seed(false).unwrap();
-	let key_id = ExtKeychainPath::new(1, 1, 0, 0, 0).to_identifier();
+	let chain = mine_chain(chain_dir, 4);
 
-	let chain_store = Arc::new(chain::store::ChainStore::new(chain_dir).unwrap());
+	// Check head exists in the db.
+	assert_eq!(chain.head().unwrap().height, 3);
 
-	global::set_mining_mode(ChainTypes::AutomatedTesting);
-	let genesis = pow::mine_genesis_block().unwrap();
+	// Check the header exists in the db.
+	assert_eq!(chain.head_header().unwrap().height, 3);
 
-	setup_chain(&genesis, chain_store.clone()).unwrap();
+	// Check header_by_height index.
+	let block_header = chain.get_header_by_height(3).unwrap();
+	let block_hash = block_header.hash();
+	assert_eq!(block_hash, chain.head().unwrap().last_block_h);
 
-	let reward = libtx::reward::output(
-		&keychain,
-		&libtx::ProofBuilder::new(&keychain),
-		&key_id,
-		0,
-		0,
-		false,
-	)
-	.unwrap();
-	let block = Block::new(&genesis.header, vec![], Difficulty::min(), reward).unwrap();
-	let block_hash = block.hash();
-
-	{
-		let batch = chain_store.batch().unwrap();
-		batch.save_block_header(&block.header).unwrap();
-		batch.save_block(&block).unwrap();
-		batch.commit().unwrap();
-	}
-
-	let block_header = chain_store.get_block_header(&block_hash).unwrap();
-	assert_eq!(block_header.hash(), block_hash);
-
-	// Test we can retrive the block from the db and that we can safely delete the
-	// block from the db even though the block_sums are missing.
 	{
 		// Block exists in the db.
-		assert!(chain_store.get_block(&block_hash).is_ok());
+		assert_eq!(chain.get_block(&block_hash).unwrap().hash(), block_hash);
 
-		// Block sums do not exist (we never set them up).
-		assert!(chain_store.get_block_sums(&block_hash).is_err());
+		// Check we have block_sums in the db.
+		assert!(chain.get_block_sums(&block_hash).is_ok());
 
 		{
 			// Start a new batch and delete the block.
-			let batch = chain_store.batch().unwrap();
+			let store = chain.store();
+			let batch = store.batch().unwrap();
 			assert!(batch.delete_block(&block_hash).is_ok());
 
 			// Block is deleted within this batch.
@@ -101,8 +59,9 @@ fn test_various_store_indices() {
 		}
 
 		// Check the batch did not commit any changes to the store .
-		assert!(chain_store.get_block(&block_hash).is_ok());
+		assert!(chain.get_block(&block_hash).is_ok());
 	}
+
 	// Cleanup chain directory
 	clean_output_dir(chain_dir);
 }