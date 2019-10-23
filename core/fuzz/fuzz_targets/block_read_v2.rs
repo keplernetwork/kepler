mwcproject/mwc-node

Support protocol version 1 and 2 in fuzz targets (#3073)

core/fuzz/fuzz_targets/block_read_v2.rs
@@ -3,10 +3,10 @@ extern crate grin_core;
 #[macro_use]
 extern crate libfuzzer_sys;
 
-use grin_core::core::CompactBlock;
+use grin_core::core::Block;
 use grin_core::ser;
 
 fuzz_target!(|data: &[u8]| {
 	let mut d = data.clone();
-	let _t: Result<CompactBlock, ser::Error> = ser::deserialize(&mut d);
+	let _t: Result<Block, ser::Error> = ser::deserialize(&mut d, ser::ProtocolVersion(2));
 });

core/fuzz/fuzz_targets/transaction_read_v2.rs
@@ -1,0 +1,12 @@
+#![no_main]
+extern crate grin_core;
+#[macro_use]
+extern crate libfuzzer_sys;
+
+use grin_core::core::Transaction;
+use grin_core::ser;
+
+fuzz_target!(|data: &[u8]| {
+	let mut d = data.clone();
+	let _t: Result<Transaction, ser::Error> = ser::deserialize(&mut d, ser::ProtocolVersion(2));
+});

keplernetwork/kepler

core/fuzz/fuzz_targets/block_read_v2.rs
@@ -3,10 +3,10 @@ extern crate kepler_core;
 #[macro_use]
 extern crate libfuzzer_sys;
 
-use kepler_core::core::CompactBlock;
+use kepler_core::core::Block;
 use kepler_core::ser;
 
 fuzz_target!(|data: &[u8]| {
 	let mut d = data.clone();
-	let _t: Result<CompactBlock, ser::Error> = ser::deserialize(&mut d);
+	let _t: Result<Block, ser::Error> = ser::deserialize(&mut d, ser::ProtocolVersion(2));
 });

core/fuzz/fuzz_targets/transaction_read_v2.rs
@@ -1,0 +1,12 @@
+#![no_main]
+extern crate kepler_core;
+#[macro_use]
+extern crate libfuzzer_sys;
+
+use kepler_core::core::Transaction;
+use kepler_core::ser;
+
+fuzz_target!(|data: &[u8]| {
+	let mut d = data.clone();
+	let _t: Result<Transaction, ser::Error> = ser::deserialize(&mut d, ser::ProtocolVersion(2));
+});
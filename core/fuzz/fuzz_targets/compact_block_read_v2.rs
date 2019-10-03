34e288673849ad2ed515db945b1a7c2c0fc7378a
hashmap <hashmap@gmail.com>
2019-10-03 09:08:40 +0200

    Support protocol version 1 and 2 in fuzz targets (#3073)

--- core/fuzz/fuzz_targets/compact_block_read_v2.rs
+++ core/fuzz/fuzz_targets/compact_block_read_v2.rs
@@ -3,10 +3,10 @@ extern crate kepler_core;
 #[macro_use]
 extern crate libfuzzer_sys;
 
-use kepler_core::core::Transaction;
+use kepler_core::core::CompactBlock;
 use kepler_core::ser;
 
 fuzz_target!(|data: &[u8]| {
 	let mut d = data.clone();
-	let _t: Result<Transaction, ser::Error> = ser::deserialize(&mut d);
+	let _t: Result<CompactBlock, ser::Error> = ser::deserialize(&mut d, ser::ProtocolVersion(2));
 });
