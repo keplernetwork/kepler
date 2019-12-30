mwcproject/mwc-node

Refactor transaction building combinators  (#3057)
* tx combinators now take operate on Result to allow for more robust errors handling
replace with_fee() and with_lock_height() with a more flexible with_features()

* pass kernel features in as arg to build::transaction()

* fix chain tests

* fix pool tests

* do not pass kernel around in the combinators
just set it once on the tx when building

* build::partial_transaction now takes a existing tx to build on

core/tests/common.rs
@@ -15,12 +15,9 @@
 //! Common test functions
 
 use crate::keychain::{Identifier, Keychain};
-use grin_core::core::{
-	block::{Block, BlockHeader},
-	Transaction,
-};
+use grin_core::core::{Block, BlockHeader, KernelFeatures, Transaction};
 use grin_core::libtx::{
-	build::{self, input, output, with_fee},
+	build::{self, input, output},
 	proof::{ProofBuild, ProofBuilder},
 	reward,
 };
@@ -36,12 +33,8 @@ pub fn tx2i1o() -> Transaction {
 	let key_id3 = keychain::ExtKeychain::derive_key_id(1, 3, 0, 0, 0);
 
 	build::transaction(
-		vec![
-			input(10, key_id1),
-			input(11, key_id2),
-			output(19, key_id3),
-			with_fee(2),
-		],
+		KernelFeatures::Plain { fee: 2 },
+		vec![input(10, key_id1), input(11, key_id2), output(19, key_id3)],
 		&keychain,
 		&builder,
 	)
@@ -56,7 +49,8 @@ pub fn tx1i1o() -> Transaction {
 	let key_id2 = keychain::ExtKeychain::derive_key_id(1, 2, 0, 0, 0);
 
 	build::transaction(
-		vec![input(5, key_id1), output(3, key_id2), with_fee(2)],
+		KernelFeatures::Plain { fee: 2 },
+		vec![input(5, key_id1), output(3, key_id2)],
 		&keychain,
 		&builder,
 	)
@@ -74,12 +68,8 @@ pub fn tx1i2o() -> Transaction {
 	let key_id3 = keychain::ExtKeychain::derive_key_id(1, 3, 0, 0, 0);
 
 	build::transaction(
-		vec![
-			input(6, key_id1),
-			output(3, key_id2),
-			output(1, key_id3),
-			with_fee(2),
-		],
+		KernelFeatures::Plain { fee: 2 },
+		vec![input(6, key_id1), output(3, key_id2), output(1, key_id3)],
 		&keychain,
 		&builder,
 	)
@@ -124,7 +114,8 @@ where
 	B: ProofBuild,
 {
 	build::transaction(
-		vec![input(v, key_id1), output(3, key_id2), with_fee(2)],
+		KernelFeatures::Plain { fee: 2 },
+		vec![input(v, key_id1), output(3, key_id2)],
 		keychain,
 		builder,
 	)

keplernetwork/kepler

core/tests/common.rs
@@ -15,12 +15,9 @@
 //! Common test functions
 
 use crate::keychain::{Identifier, Keychain};
-use kepler_core::core::{
-	block::{Block, BlockHeader},
-	Transaction,
-};
+use kepler_core::core::{Block, BlockHeader, KernelFeatures, Transaction};
 use kepler_core::libtx::{
-	build::{self, input, output, with_fee},
+	build::{self, input, output},
 	proof::{ProofBuild, ProofBuilder},
 	reward,
 };
@@ -36,12 +33,8 @@ pub fn tx2i1o() -> Transaction {
 	let key_id3 = keychain::ExtKeychain::derive_key_id(1, 3, 0, 0, 0);
 
 	build::transaction(
-		vec![
-			input(10, key_id1),
-			input(11, key_id2),
-			output(19, key_id3),
-			with_fee(2),
-		],
+		KernelFeatures::Plain { fee: 2 },
+		vec![input(10, key_id1), input(11, key_id2), output(19, key_id3)],
 		&keychain,
 		&builder,
 	)
@@ -56,7 +49,8 @@ pub fn tx1i1o() -> Transaction {
 	let key_id2 = keychain::ExtKeychain::derive_key_id(1, 2, 0, 0, 0);
 
 	build::transaction(
-		vec![input(5, key_id1), output(3, key_id2), with_fee(2)],
+		KernelFeatures::Plain { fee: 2 },
+		vec![input(5, key_id1), output(3, key_id2)],
 		&keychain,
 		&builder,
 	)
@@ -74,12 +68,8 @@ pub fn tx1i2o() -> Transaction {
 	let key_id3 = keychain::ExtKeychain::derive_key_id(1, 3, 0, 0, 0);
 
 	build::transaction(
-		vec![
-			input(6, key_id1),
-			output(3, key_id2),
-			output(1, key_id3),
-			with_fee(2),
-		],
+		KernelFeatures::Plain { fee: 2 },
+		vec![input(6, key_id1), output(3, key_id2), output(1, key_id3)],
 		&keychain,
 		&builder,
 	)
@@ -125,7 +115,8 @@ where
 	B: ProofBuild,
 {
 	build::transaction(
-		vec![input(v, key_id1), output(3, key_id2), with_fee(2)],
+		KernelFeatures::Plain { fee: 2 },
+		vec![input(v, key_id1), output(3, key_id2)],
 		keychain,
 		builder,
 	)