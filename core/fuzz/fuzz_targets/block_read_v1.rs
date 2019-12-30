mwcproject/mwc-node

Verify headers and blocks only when needed (#3023)
* Verify headers and blocks only when needed

Curretnly we have some lightweigt validation implemented as part of
entity deserialization, which is safer and allows us to not parse the
entire object if some part is invalid. At the same time this logic
always applies when we read an entity, eg when reading from DB.

This PR introduces UntrustedHeader/Block which is used when we read from
the network. It does partial validation during read, then it is supposed
to be converted into regular header/block which doesn't validate itself.

Also this PR adds "lightweight" validation to block header read like we have
for block body, so we don't parse block body if the header is invalid.

Fixes #1642

* Move version validation to untrusted header

* update fuzz tests

core/fuzz/fuzz_targets/block_read_v1.rs
@@ -3,10 +3,10 @@ extern crate grin_core;
 #[macro_use]
 extern crate libfuzzer_sys;
 
-use grin_core::core::Block;
+use grin_core::core::UntrustedBlock;
 use grin_core::ser;
 
 fuzz_target!(|data: &[u8]| {
 	let mut d = data.clone();
-	let _t: Result<Block, ser::Error> = ser::deserialize(&mut d, ser::ProtocolVersion(1));
+	let _t: Result<UntrustedBlock, ser::Error> = ser::deserialize(&mut d, ser::ProtocolVersion(1));
 });

core/fuzz/fuzz_targets/block_read_v2.rs
@@ -3,10 +3,10 @@ extern crate grin_core;
 #[macro_use]
 extern crate libfuzzer_sys;
 
-use grin_core::core::Block;
+use grin_core::core::UntrustedBlock;
 use grin_core::ser;
 
 fuzz_target!(|data: &[u8]| {
 	let mut d = data.clone();
-	let _t: Result<Block, ser::Error> = ser::deserialize(&mut d, ser::ProtocolVersion(2));
+	let _t: Result<UntrustedBlock, ser::Error> = ser::deserialize(&mut d, ser::ProtocolVersion(2));
 });

core/fuzz/fuzz_targets/compact_block_read_v1.rs
@@ -3,10 +3,11 @@ extern crate grin_core;
 #[macro_use]
 extern crate libfuzzer_sys;
 
-use grin_core::core::CompactBlock;
+use grin_core::core::UntrustedCompactBlock;
 use grin_core::ser;
 
 fuzz_target!(|data: &[u8]| {
 	let mut d = data.clone();
-	let _t: Result<CompactBlock, ser::Error> = ser::deserialize(&mut d, ser::ProtocolVersion(1));
+	let _t: Result<UntrustedCompactBlock, ser::Error> =
+		ser::deserialize(&mut d, ser::ProtocolVersion(1));
 });

core/fuzz/fuzz_targets/compact_block_read_v2.rs
@@ -3,10 +3,11 @@ extern crate grin_core;
 #[macro_use]
 extern crate libfuzzer_sys;
 
-use grin_core::core::CompactBlock;
+use grin_core::core::UntrustedCompactBlock;
 use grin_core::ser;
 
 fuzz_target!(|data: &[u8]| {
 	let mut d = data.clone();
-	let _t: Result<CompactBlock, ser::Error> = ser::deserialize(&mut d, ser::ProtocolVersion(2));
+	let _t: Result<UntrustedCompactBlock, ser::Error> =
+		ser::deserialize(&mut d, ser::ProtocolVersion(2));
 });

keplernetwork/kepler

core/fuzz/fuzz_targets/block_read_v1.rs
@@ -3,10 +3,10 @@ extern crate kepler_core;
 #[macro_use]
 extern crate libfuzzer_sys;
 
-use kepler_core::core::Block;
+use kepler_core::core::UntrustedBlock;
 use kepler_core::ser;
 
 fuzz_target!(|data: &[u8]| {
 	let mut d = data.clone();
-	let _t: Result<Block, ser::Error> = ser::deserialize(&mut d, ser::ProtocolVersion(1));
+	let _t: Result<UntrustedBlock, ser::Error> = ser::deserialize(&mut d, ser::ProtocolVersion(1));
 });

core/fuzz/fuzz_targets/block_read_v2.rs
@@ -3,10 +3,10 @@ extern crate kepler_core;
 #[macro_use]
 extern crate libfuzzer_sys;
 
-use kepler_core::core::Block;
+use kepler_core::core::UntrustedBlock;
 use kepler_core::ser;
 
 fuzz_target!(|data: &[u8]| {
 	let mut d = data.clone();
-	let _t: Result<Block, ser::Error> = ser::deserialize(&mut d, ser::ProtocolVersion(2));
+	let _t: Result<UntrustedBlock, ser::Error> = ser::deserialize(&mut d, ser::ProtocolVersion(2));
 });

core/fuzz/fuzz_targets/compact_block_read_v1.rs
@@ -3,10 +3,11 @@ extern crate kepler_core;
 #[macro_use]
 extern crate libfuzzer_sys;
 
-use kepler_core::core::CompactBlock;
+use kepler_core::core::UntrustedCompactBlock;
 use kepler_core::ser;
 
 fuzz_target!(|data: &[u8]| {
 	let mut d = data.clone();
-	let _t: Result<CompactBlock, ser::Error> = ser::deserialize(&mut d, ser::ProtocolVersion(1));
+	let _t: Result<UntrustedCompactBlock, ser::Error> =
+		ser::deserialize(&mut d, ser::ProtocolVersion(1));
 });

core/fuzz/fuzz_targets/compact_block_read_v2.rs
@@ -3,10 +3,11 @@ extern crate kepler_core;
 #[macro_use]
 extern crate libfuzzer_sys;
 
-use kepler_core::core::CompactBlock;
+use kepler_core::core::UntrustedCompactBlock;
 use kepler_core::ser;
 
 fuzz_target!(|data: &[u8]| {
 	let mut d = data.clone();
-	let _t: Result<CompactBlock, ser::Error> = ser::deserialize(&mut d, ser::ProtocolVersion(2));
+	let _t: Result<UntrustedCompactBlock, ser::Error> =
+		ser::deserialize(&mut d, ser::ProtocolVersion(2));
 });
4bcd84f18e738fbc78a88d1323b9bce27fdbff4a
hashmap <hashmap@gmail.com>
2019-10-27 08:40:52 +0100

    Verify headers and blocks only when needed (#3023)
    * Verify headers and blocks only when needed
    
    Curretnly we have some lightweigt validation implemented as part of
    entity deserialization, which is safer and allows us to not parse the
    entire object if some part is invalid. At the same time this logic
    always applies when we read an entity, eg when reading from DB.
    
    This PR introduces UntrustedHeader/Block which is used when we read from
    the network. It does partial validation during read, then it is supposed
    to be converted into regular header/block which doesn't validate itself.
    
    Also this PR adds "lightweight" validation to block header read like we have
    for block body, so we don't parse block body if the header is invalid.
    
    Fixes #1642
    
    * Move version validation to untrusted header
    
    * update fuzz tests

--- core/fuzz/fuzz_targets/block_read_v2.rs
+++ core/fuzz/fuzz_targets/block_read_v2.rs
@@ -3,10 +3,10 @@ extern crate kepler_core;
 #[macro_use]
 extern crate libfuzzer_sys;
 
-use kepler_core::core::Block;
+use kepler_core::core::UntrustedBlock;
 use kepler_core::ser;
 
 fuzz_target!(|data: &[u8]| {
 	let mut d = data.clone();
-	let _t: Result<Block, ser::Error> = ser::deserialize(&mut d, ser::ProtocolVersion(2));
+	let _t: Result<UntrustedBlock, ser::Error> = ser::deserialize(&mut d, ser::ProtocolVersion(2));
 });
